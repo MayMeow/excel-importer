93143e23698ad35c8e04d93d323a8dd037f802e8
May Meow <3164256+MayMeow@users.noreply.github.com>
2025-03-23 21:00:53 +0100

    Rename Required to NotEmpty

--- src/Attributes/NotEmpty.php
+++ src/Attributes/NotEmpty.php
@@ -5,7 +5,7 @@ namespace MayMeow\ExcelImporter\Attributes;
 use MayMeow\ExcelImporter\Validators\ValidatorAttributeInterface;
 
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
-class Required implements ValidatorAttributeInterface
+class NotEmpty extends BaseValidatorAttribute implements ValidatorAttributeInterface
 {
     public function validate(mixed $value): bool
     {
