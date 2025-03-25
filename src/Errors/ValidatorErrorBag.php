444fbfdb7f915e5da4ea92286de3cc21e6da1c3d
May <3164256+MayMeow@users.noreply.github.com>
2025-03-25 09:36:42 +0100

    update error bag and validator

--- src/Errors/ValidatorErrorBag.php
+++ src/Errors/ValidatorErrorBag.php
@@ -22,14 +22,63 @@ class ValidatorErrorBag
         return $field ? ($this->errors[$field] ?? []) : $this->errors;
     }
 
-    public function getFirstError(?string $field = null): ?string
+    public function getFirstError(?string $field = null, ?int $index = null): ?string
     {
-        if ($field) {
-            return $this->errors[$field][0] ?? null;
+
+        // For provided Index and field
+        if ($index !== null) {
+
+            // Index is not exists return null
+            if (!isset($this->errors[$index])) {
+                return null;
+            }
+
+            // Index and field are provided
+            if ($field !== null) {
+                return $this->errors[$index][$field][0] ?? null;
+            }
+
+            // filed is not provided, return first error from index from any field
+            foreach ($this->errors[$index] ?? [] as $message) {
+                return $message[0] ?? null;
+            }
         }
 
-        foreach ($this->errors as $message) {
-            return $message[0];
+        // for provided field
+        if ($field !== null) {
+            // if field exists and is not list
+            if (isset($this->errors[$field]) && !array_is_list($this->errors[$field])) {
+                return $this->errors[$field][0] ?? null;
+            }
+
+            if (!isset($this->errors[$field])) {
+                // field error not found, try to find in indexed array
+                // This is case when you testing multiple models (array of models) and trying to find error for specific field
+                // try iterate ofver array and check if filed exists in member arrays
+                foreach ($this->errors as $fieldErrors) {
+                    if (isset($fieldErrors[$field])) {
+                        return $fieldErrors[$field][0] ?? null;
+                    }
+                }
+
+                return null; // filed error not found
+            }
+            // othervise GO TO general case
+        }
+
+        // general case: get first error from any field (entrie set)
+        foreach ($this->errors as $_ => $fieldErrors) {
+            if (is_array($fieldErrors)) {
+                foreach ($fieldErrors as $errorGroup) {
+                    if (is_array($errorGroup)) {
+                        return $errorGroup[0] ?? null;
+                    } else {
+                        return $errorGroup;
+                    }
+                }
+            } else {
+                return $fieldErrors;
+            }
         }
 
         return null;
--- tests/FileImportTest.php
+++ tests/FileImportTest.php
@@ -1,9 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace MayMeow\ExcelImporter\Test;
 
 use Exception;
 use MayMeow\ExcelImporter\Attributes\NotEmpty;
+use MayMeow\ExcelImporter\Errors\ValidatorErrorBag;
 use MayMeow\ExcelImporter\Exceptions\ValidationException;
 use MayMeow\ExcelImporter\Test\Models\TestingModel;
 use MayMeow\ExcelImporter\Test\Tools\TestingDataLocator;
@@ -15,44 +16,56 @@ use PHPUnit\Framework\TestCase;
 
 class FileImportTest extends TestCase
 {
-    /** @test */
-    public function testImportingFile()
+    public function __construct(protected array $modelArray = [])
     {
+        parent::__construct();
+
         $xlsxReader = new Xlsx();
         $spreadsheet = $xlsxReader->load((new TestingDataLocator())->locateExcelFile());
         $writer = new ModelWriter();
 
         /** @var array<TestingModel> $modelArray */
-        $modelArray = $writer->write(TestingModel::class, $spreadsheet);
+        $this->modelArray = $writer->write(TestingModel::class, $spreadsheet);
+    }
 
+    /** @test */
+    public function testImportingFile()
+    {
+        $this->assertEquals('a', $this->modelArray[0]->getColA());
+        $this->assertEquals('g', $this->modelArray[0]->getColG());
 
-        $this->assertEquals('a', $modelArray[0]->getColA());
-        $this->assertEquals('g', $modelArray[0]->getColG());
+        $this->assertEquals(6, $this->modelArray[1]->getColF());
+    }
 
-        $this->assertEquals(6, $modelArray[1]->getColF());
+    /** @test */
+    public function testThrowingExceptionForMany()
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('Validation failed');
+
+        $baseValidator = new BaseValidator(failFast: false, throwException: true);
+        $baseValidator->validateMany($this->modelArray, rule: NotEmpty::class);
+        $baseValidator->validate($this->modelArray[2], rule: NotEmpty::class);
+    }
+
+    /** @test */
+    public function testThrowingException()
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('Validation failed');
+
+        $baseValidator = new BaseValidator(failFast: false, throwException: true);
+        $baseValidator->validate($this->modelArray[2], rule: NotEmpty::class);
     }
 
     /** @test */
     public function testImportingModelOnLineTwoShouldFail()
     {
-        #$this->expectException(ValidationException::class);
-        #$this->expectExceptionMessage('Validation failed');
-
-        $xlsxReader = new Xlsx();
-        $spreadsheet = $xlsxReader->load((new TestingDataLocator())->locateExcelFile());
-        $writer = new ModelWriter();
-
-        /** @var array<TestingModel> $modelArray */
-        $modelArray = $writer->write(TestingModel::class, $spreadsheet);
-
         $baseValidator = new BaseValidator(failFast: false, throwException: true);
 
-        #$baseValidator->validateMany($modelArray, rule: NotEmpty::class);
-
         try {
-            $baseValidator->validateMany($modelArray, rule: NotEmpty::class);
+            $baseValidator->validateMany($this->modelArray, rule: NotEmpty::class);
         } catch (ValidationException $e) {
-            var_dump($e->getErrors());
             $this->assertEquals('Property colA is required', $e->getErrors()->getFirstError());
         }
     }
@@ -62,15 +75,39 @@ class FileImportTest extends TestCase
     {
         $this->expectNotToPerformAssertions();
 
-        $xlsxReader = new Xlsx();
-        $spreadsheet = $xlsxReader->load((new TestingDataLocator())->locateExcelFile());
-        $writer = new ModelWriter();
-
-        /** @var array<TestingModel> $modelArray */
-        $modelArray = $writer->write(TestingModel::class, $spreadsheet);
-
         $baseValidator = new BaseValidator(failFast: true, throwException: true);
 
-        $baseValidator->validate($modelArray[1], rule: NotEmpty::class);
+        $baseValidator->validate($this->modelArray[1], rule: NotEmpty::class);
+    }
+
+    /** @test */
+    public function testValidatorForMany()
+    {
+        $baseValidator = new BaseValidator(failFast: true, throwException: false);
+
+        /** @var ValidatorErrorBag $e */
+        $e = $baseValidator->validateMany($this->modelArray, rule: NotEmpty::class);
+
+        if ($e->hasErrors()) {
+            $this->assertEquals('Property colA is required', $e->getFirstError()); // return any first error
+            $this->assertEquals('Property colA is required', $e->getFirstError(index: 2)); // return any first error on index 2
+            $this->assertEquals('Property colA is required', $e->getFirstError(field: 'colA')); // return first error for field colA in any index
+            $this->assertEquals('Property colA is required', $e->getFirstError(field: 'colA', index: 2)); // return first error for field colA on index 2
+            $this->assertEquals(null, $e->getFirstError(index: 1)); // row 1 should be valid
+        }
+    }
+
+    /** @test */
+    public function testValidation()
+    {
+        $baseValidator = new BaseValidator(failFast: true, throwException: false);
+
+        $e = $baseValidator->validate($this->modelArray[2], rule: NotEmpty::class);
+
+        if ($e->hasErrors()) {
+            $this->assertEquals('Property colA is required', $e->getFirstError()); // first error for any field
+            $this->assertEquals('Property colA is required', $e->getFirstError(field: 'colA')); // first error for field colA
+            $this->assertEquals(null, $e->getFirstError(field: 'colB')); // field colB -> there should be no error
+        }
     }
 }
